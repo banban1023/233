de95ef2edd585a31536e6369a0fd9e89aa783042
板板 <747657490@qq.com>
2025-04-04 11:51:26 +0800

    瀑布流

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <WaterfallCard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WaterfallCard from './components/WaterfallCard.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WaterfallCard
   }
 }
 </script>
 
 <style lang="less">
+body {
+  box-sizing: border-box;
+  margin: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  background-color: #f5f5f5;
+  padding-top: 10px;
 }
 </style>
---
+++ src/components/WaterfallCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="waterfall">
+      <div v-for="item in list"
+      :key="item.id"
+      class="card"
+      :style="{ height: item.height + 'px' }"
+      >{{ item.id }}</div>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'WaterfallCard',
+  data () {
+    return {
+      list: [
+        { id: 1, height: 50 },
+        { id: 2, height: 70 },
+        { id: 3, height: 60 },
+        { id: 4, height: 30 },
+        { id: 5, height: 50 },
+        { id: 6, height: 70 }
+      ]
+    }
+  },
+  mounted () {
+    const cards = document.querySelectorAll('.card')
+    Array.from(cards).forEach(element => {
+      const row = Math.ceil(element.clientHeight / 5) + 2
+      element.style.gridRowEnd = 'span ' + row
+    })
+  }
+}
+
+</script>
+<style lang="less" scoped>
+.waterfall {
+    box-sizing: border-box;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(2, calc(50% - 8px));  // 修改为两列布局
+    align-items: start;
+    justify-content: space-evenly;
+    grid-auto-rows: 5px;
+    .card {
+        background-color: pink;
+        padding: 7px; // 添加底部间距
+    }
+}
+  </style>
